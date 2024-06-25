a831c4682c20e332e139c6b6cc605a06ec88a2c3
tolibaevRinat <webtolibaevrinat@gmail.com>
2024-06-25 20:06:38 +0500

    25-06--03

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,15 +1,18 @@
-import { Accordion, AccordionItem } from "@szhsin/react-accordion";
-import styles from "./footer.module.scss";
+import { Accordion, AccordionItem } from '@szhsin/react-accordion';
+import { useMediaQuery } from 'react-responsive';
+
+import styles from './footer.module.scss';
 
 export const Footer = () => {
+  const isMobile = useMediaQuery({ minWidth: 767.98 });
   return (
     <section className={`${styles.root}`}>
-      <div className={`${styles.body} container`}>
+      <div className={`${styles.body} container flex f-d-col`}>
         <a className={`${styles.logo}`} href="#!">
           Gerey-Tuz
         </a>
-        <Accordion>
-          <AccordionItem header="Навигация">
+        <Accordion transition transitionTimeout={300}>
+          <AccordionItem header="Навигация" disabled={isMobile} initialEntered={isMobile}>
             <ul>
               <li>
                 <a href="#!">О нас</a>
@@ -18,30 +21,59 @@ export const Footer = () => {
                 <a href="#!">Деятельность</a>
               </li>
               <li>
-                <a href="#!"></a>
+                <a href="#!">Продукция</a>
               </li>
               <li>
-                <a href="#!"></a>
+                <a href="#!">Отзывы</a>
               </li>
             </ul>
-            <a className={`${styles.item}`}></a>
           </AccordionItem>
 
-          <AccordionItem header="Where does it come from?">
-            <a className={`${styles.item}`}>
-              Quisque eget luctus mi, vehicula mollis lorem. Proin fringilla vel
-              erat quis sodales. Nam ex enim, eleifend venenatis lectus vitae,
-              accumsan auctor mi.
-            </a>
+          <AccordionItem header="Адрес" disabled={isMobile} initialEntered={isMobile}>
+            <ul>
+              <li>
+                <a
+                  className={`${styles.w} flex f-d-col`}
+                  href="https://maps.app.goo.gl/pMiWnSZgTeUp3EW29"
+                  target="_blank"
+                >
+                  <span>368530, Республика Дагестан,</span>
+                  <span>Карабудахкентский район,</span>
+                  <span>с. Карабудахкент,</span>
+                  <span>ул. Иновова, дом 23.</span>
+                </a>
+              </li>
+            </ul>
           </AccordionItem>
 
-          <AccordionItem header="Why do we use it?">
-            <a className={`${styles.item}`}>
-              Suspendisse massa risus, pretium id interdum in, dictum sit amet
-              ante. Fusce vulputate purus sed tempus feugiat.
-            </a>
+          <AccordionItem header="Контакты" disabled={isMobile} initialEntered={isMobile}>
+            <ul>
+              <li>
+                <a href="tel:792899623931">+7 928 9962-39-31</a>
+              </li>
+              <li>
+                <a href="mailto:info@gerey-tuz.ru">info@gerey-tuz.ru</a>
+              </li>
+              <li>
+                <a href="tel:792899623931">Обратная связь</a>
+              </li>
+            </ul>
+          </AccordionItem>
+
+          <AccordionItem header="Контакты" disabled={isMobile} initialEntered={isMobile}>
+            <ul>
+              <li>
+                <a href="#!">Вконтакте</a>
+              </li>
+              <li>
+                <a href="#!">Telegram</a>
+              </li>
+            </ul>
           </AccordionItem>
         </Accordion>
+        <h3 className={`${styles.subtitle}`}>
+          @2020 ООО Агрофирма «Герей-Тюз». Все права защищены.
+        </h3>
       </div>
     </section>
   );
